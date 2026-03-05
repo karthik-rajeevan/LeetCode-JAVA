c14a0daabc1904724383be3bd76c6d4e7d5702d1
karthik-rajeevan <141824634+karthik-rajeevan@users.noreply.github.com>
2026-03-06 00:46:57 +0530

    Time: 1 ms (68.29%), Space: 44.3 MB (56.35%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int res=0;
+        for(String s : details){
+            if(Integer.parseInt(s.substring(11,13))>60){
+                res++;
+            }
+        }
+        return res;
+        
+    }
+}
